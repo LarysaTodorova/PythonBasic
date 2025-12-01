4c3b496cc7d504d76e99a0b0ba0105f7c23161a3
Lora <lorageo33@gmail.com>
2025-12-01 16:27:38 +0200

    Initial commit

--- lesson14/homework/gpt01.py
+++ lesson14/homework/gpt01.py
@@ -6,4 +6,22 @@
 #   Функция должна работать с любыми значениями
 
 def find_max_numbers(numbers):
-    max_numbers = []
+    max_index = 0
+    for i in range(len(numbers)):
+        if numbers[i] > numbers[max_index]:
+            max_index = i
+
+    second_max_number = None
+    for i in range(len(numbers)):
+        if i == max_index:
+            continue
+        if second_max_number is None or numbers[i] > numbers[second_max_number]:
+            second_max_number = i
+
+    numbers[max_index], numbers[second_max_number] = numbers[second_max_number], numbers[max_index]
+
+    return numbers
+
+
+array_numbers = [8, -1, 100, 500, 5, 0, -6, 3]
+print(find_max_numbers(array_numbers))
